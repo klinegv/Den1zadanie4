1d6a786c3b041e6d912e0b76074a0c1cca775748
Alex Genov <alexgenov@MacBook-Pro-Alex.local>
2018-01-24 02:40:33 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.SortedMap;
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        int a = 12;                   //// вводим данные
+        int b = 9;
+
+        int p = a + b + a + b;        //// формула периметра
+        System.out.println(p);
+
+        int s = a * b;                //// формула площади
+        System.out.print(s);
+    }
+}
+
